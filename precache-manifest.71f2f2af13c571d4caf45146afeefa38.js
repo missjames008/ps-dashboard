b84011588d9b0e72fc41531bb953fe506d51f4e7
Jocelyn James <jocelynjames@Jocelyns-MacBook-Pro.local>
2020-07-27 13:17:28 -0400

    Updates

---
+++ precache-manifest.71f2f2af13c571d4caf45146afeefa38.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd46b5a411d07c32fb4237a2a1623b8",
+    "url": "/ps-dashboard/index.html"
+  },
+  {
+    "revision": "a6a6ccf5f2e6d435edee",
+    "url": "/ps-dashboard/static/css/2.d9c29e73.chunk.css"
+  },
+  {
+    "revision": "fc11efd3e9740c9e8d7b",
+    "url": "/ps-dashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a6a6ccf5f2e6d435edee",
+    "url": "/ps-dashboard/static/js/2.69708381.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/ps-dashboard/static/js/2.69708381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc11efd3e9740c9e8d7b",
+    "url": "/ps-dashboard/static/js/main.beb876f8.chunk.js"
+  },
+  {
+    "revision": "bede4a8678e1e595c873",
+    "url": "/ps-dashboard/static/js/runtime-main.9e7c3626.js"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/ps-dashboard/static/media/brand-icons.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/ps-dashboard/static/media/brand-icons.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/ps-dashboard/static/media/brand-icons.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/ps-dashboard/static/media/brand-icons.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/ps-dashboard/static/media/brand-icons.ec3cfdde.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ps-dashboard/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/ps-dashboard/static/media/icons.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/ps-dashboard/static/media/icons.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/ps-dashboard/static/media/icons.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/ps-dashboard/static/media/icons.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/ps-dashboard/static/media/icons.bea989e8.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/ps-dashboard/static/media/outline-icons.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/ps-dashboard/static/media/outline-icons.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/ps-dashboard/static/media/outline-icons.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/ps-dashboard/static/media/outline-icons.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/ps-dashboard/static/media/outline-icons.f89ea91e.woff"
+  }
+]);
